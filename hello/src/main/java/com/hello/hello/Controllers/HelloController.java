7787d2a49aebb556da7e432132798dfe3a78139a
Dipak Ugale <ugale.deepak3010@gmail.com>
2024-09-04 16:27:53 +0000

    adding all

---
+++ hello/src/main/java/com/hello/hello/Controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.hello.hello.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+
+        return "Hello Deepak";
+    }
+    
+}
